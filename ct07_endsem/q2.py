421397c1f7c757901f3f589b24611aba271d3de4
Elijah2107 <olgawong@hotmail.com>
2026-05-03 08:52:50 +0000

    EndSem2

---
+++ ct07_endsem/q2.py
@@ -0,0 +1,49 @@
+# """
+# ============================================================
+# Q2. Odd or Even Checker
+# ============================================================
+# You are given a list of numbers.
+# The program must check whether each number is odd or even.
+
+# num_list = [2944, -5490, 2357, -2619, 1177, 451, -8299, 2533, 4682, -6040, 0]
+
+# Program Requirements:
+# - Write a function is_even(num)
+# - If the number is even, return True
+# - If the number is odd, return False
+# - Use a for loop to go through the list
+# - Call the function for each number
+
+# Print the result exactly in this format:
+#     2944 is even.
+#     -2619 is odd.
+#     ...
+
+# ============================================================
+# """
+
+# ============================================================
+# Step 1: Create the function
+# ============================================================
+def is_even(num):
+    if num % 2 == 0:
+        return True
+    else:
+        return False
+    
+
+# ============================================================
+# Step 2: Create the list
+# ============================================================
+num_list = [2944, -5490, 2357, -2619, 1177, 451, -8299, 2533, 4682, -6040, 0]
+
+
+# ============================================================
+# Step 3: Loop through the list and use the function
+# ============================================================
+for number in num_list:
+    answer = is_even(number)
+    if number == answer:
+        print(str(number) + " is even.")
+    if number == answer:
+        print(str(number) + " is odd.")
